cb20ec37e329a5759cfaa6669a57244b346afe4f
Chandra Mouli Kumar <65549746+Chandramouli9105@users.noreply.github.com>
2023-10-17 21:35:02 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to DevOps-Project";
+		return "Welcome to DevOps-Project";
 	}
 
 	public static void main(String[] args) {
